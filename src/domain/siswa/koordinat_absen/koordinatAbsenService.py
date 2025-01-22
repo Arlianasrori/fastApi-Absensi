be1e78e82b57d4755cf547792ca8faffdc608ef1
arlianasrori <arlianasrori@gmail.com>
2025-01-22 14:11:24 +0800

    feat(siswa/absen) add absen service for siswa

--- src/domain/siswa/koordinat_absen/koordinatAbsenService.py
+++ src/domain/siswa/koordinat_absen/koordinatAbsenService.py
@@ -4,10 +4,14 @@ from sqlalchemy.orm import joinedload
 
 # models 
 from ....models.absen_model import KoordinatAbsenKelas
+from ....models.jadwal_model import Jadwal
 # schemas
+from .koordinatAbsenSchema import CekRadiusKoordinatRequest, CekRadiusKoordinatResponse
 from ...schemas.koordinatAbsen_schema import KoordinatAbsenKelasBase, KoordinatAbsenDetail
 # common
 from ....error.errorHandling import HttpException
+from ...common.get_distance_koordinat import calculate_radius
+from ...common.get_day_today import get_day
 
 async def getAllKoordinat(siswa : dict,session : AsyncSession) -> KoordinatAbsenKelasBase :
     findKoordinatAbsen = (await session.execute(select(KoordinatAbsenKelas).where(and_(KoordinatAbsenKelas.id_kelas == siswa["id_kelas"])))).scalars().all()
@@ -25,4 +29,24 @@ async def getKoordinatById(id : int,session : AsyncSession) -> KoordinatAbsenDet
     return {
         "msg" : "success",
         "data" : findKoordinat
+    }
+
+async def cekRadiusKoordinat(siswa : dict,body : CekRadiusKoordinatRequest,session : AsyncSession) -> CekRadiusKoordinatResponse :
+    dayToday : dict = await get_day()
+    findJadwal = (await session.execute(select(Jadwal).options(joinedload(Jadwal.koordinat)).where(and_(Jadwal.id_kelas == siswa["id_kelas"],Jadwal.hari == dayToday["day_name"])))).scalars().all()
+
+    for jadwalItem in findJadwal :
+        if await calculate_radius(body.latitude,body.longitude,jadwalItem.koordinat.latitude,jadwalItem.koordinat.longitude) <= jadwalItem.koordinat.radius_absen_meter :
+            return {
+                "msg" : "anda berada didalam radius absen",
+                "data" : {
+                    "insideRadius" : True
+                }
+            }
+        
+    return {
+        "msg" : "anda berada diluar radius absen",
+        "data" : {
+            "insideRadius" : False
+        }
     }
