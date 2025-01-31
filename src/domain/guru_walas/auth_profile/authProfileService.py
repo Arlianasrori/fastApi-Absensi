6f9f7a72f1faa2999b65468bbe7034599c18da60
arlianasrori <arlianasrori@gmail.com>
2025-01-31 14:04:57 +0800

    feat(GuruWalas/updateProfile) : add update profile for guru walas

--- src/domain/guru_walas/auth_profile/authProfileService.py
+++ src/domain/guru_walas/auth_profile/authProfileService.py
@@ -1,14 +1,23 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import func, select,and_
+from sqlalchemy import select,and_
 from sqlalchemy.orm import joinedload
+from fastapi import UploadFile
 
 # models 
-from ...schemas.guruWalas_schema import GuruWalasBase, GuruWalasDetailWithSekolah
 from ....models.guru_walas_model import GuruWalas
+# schemas
+from ...schemas.guruWalas_schema import GuruWalasBase, GuruWalasDetailWithSekolah, GuruWalasWithAlamat
+from .authProfileSchema import UpdateProfileRequest
+from ...schemas.alamat_schema import UpdateAlamatBody
 
 # common
 from ....error.errorHandling import HttpException
-
+import aiofiles
+from python_random_strings import random_strings
+import aiofiles
+from ....utils.updateTable_util import updateTable
+import os
+from copy import deepcopy
 async def getGuruWalas(id_walas : int,session : AsyncSession) -> GuruWalasBase :
     findWalas = (await session.execute(select(GuruWalas).where(GuruWalas.id == id_walas))).scalar_one_or_none()
     if not findWalas :
@@ -27,4 +36,60 @@ async def getGuruWalasProfile(id_walas : int,session : AsyncSession) -> GuruWala
     return {
         "msg" : "success",
         "data" : findWalas
+    }
+
+async def updateProfile(id_walas : int,body : UpdateProfileRequest,alamat : UpdateAlamatBody,session : AsyncSession) -> GuruWalasWithAlamat :
+    findWalas = (await session.execute(select(GuruWalas).options(joinedload(GuruWalas.alamat)).where(GuruWalas.id == id_walas))).scalar_one_or_none()
+
+    if findWalas is None :
+        raise HttpException(404,f"Guru Walas tidak ditemukan")
+    
+    if body.model_dump(exclude_unset=True):
+        updateTable(body,findWalas)
+
+    if alamat.model_dump(exclude_unset=True):
+        updateTable(alamat,findWalas.alamat)
+
+    await session.commit()
+
+    return {
+        "msg" : "success",
+        "data" : findWalas
+    }
+
+PROFILE_STORE = os.getenv("DEV_FOTO_PROFILE_GURU_WALAS_STORE")
+PROFILE_BASE_URL = os.getenv("DEV_FOTO_PROFILE_GURU_WALAS_BASE_URL")
+
+async def add_update_foto_profile(id : int,foto_profile : UploadFile,session : AsyncSession) -> GuruWalasBase :
+    findWalas = (await session.execute(select(GuruWalas).where(GuruWalas.id == id))).scalar_one_or_none()
+    if not findWalas :
+        raise HttpException(404,f"Guru Walas tidak ditemukan")
+
+    ext_file = foto_profile.filename.split(".")
+
+    if ext_file[-1] not in ["jpg","png","jpeg"] :
+        raise HttpException(400,f"file harus berupa gambar")
+
+    file_name = f"{random_strings.random_digits(12)}-{foto_profile.filename.split(' ')[0]}.{ext_file[-1]}"
+    file_name_save = f"{PROFILE_STORE}{file_name}"
+        
+    fotoProfileBefore = findWalas.foto_profile
+
+    async with aiofiles.open(file_name_save, "wb") as f:
+        await f.write(foto_profile.file.read())
+        print(PROFILE_BASE_URL)
+        findWalas.foto_profile = f"{PROFILE_BASE_URL}/{file_name}"
+    
+    if fotoProfileBefore :
+        print(fotoProfileBefore)
+        file_nama_db_split = fotoProfileBefore.split("/")
+        file_name_db = file_nama_db_split[-1]
+        os.remove(f"{PROFILE_STORE}/{file_name_db}")
+    
+    guruWalasDictCopy = deepcopy(findWalas.__dict__)
+    await session.commit()
+
+    return {
+        "msg" : "success",
+        "data" : guruWalasDictCopy
     }
--- src/routes/guruWalasRouter.py
+++ src/routes/guruWalasRouter.py
@@ -2,14 +2,16 @@ from fastapi import APIRouter,Depends, UploadFile
 
 # auth-profile
 from ..domain.guru_walas.auth_profile import authProfileService
-from ..domain.schemas.guruWalas_schema import GuruWalasBase, GuruWalasDetailWithSekolah
+from ..domain.schemas.guruWalas_schema import GuruWalasBase, GuruWalasDetailWithSekolah, GuruWalasWithAlamat
+from ..domain.guru_walas.auth_profile.authProfileSchema import UpdateProfileRequest
+from ..domain.schemas.alamat_schema import UpdateAlamatBody
 
 # depends
 from ..auth.auth_depends.guru_walas.depend_auth_guru_walas import guruWalasDependAuth
 from ..auth.auth_depends.guru_walas.get_guru_walas_auth import getWalasAuth
 
 # common
-from ..domain.schemas.response_schema import ApiResponse,MessageOnlyResponse
+from ..domain.schemas.response_schema import ApiResponse
 from ..db.sessionDepedency import sessionDepedency
 
 guruWalasRouter = APIRouter(prefix="/guru-walas",dependencies=[Depends(guruWalasDependAuth)])
@@ -23,35 +25,10 @@ async def getguruWalas(guruWalas : dict = Depends(getWalasAuth),session : sessio
 async def getProfileguruWalas(guruWalas : dict = Depends(getWalasAuth),session : sessionDepedency = None) :
     return await authProfileService.getGuruWalasProfile(guruWalas["id"],session)
 
-# # absen
-# @petugasBKRouter.get("/absen/statistik-tinjauan",response_model=ApiResponse[StatistikAbsenResponse],tags=["PETUGASBK/ABSEN"])
-# async def getStatistikAbsen(petugasBK : dict = Depends(getPetugasBKAuth),session : sessionDepedency = None) :
-#     return await absenService.getStatistikAbsen(petugasBK["id"],session)
+@guruWalasRouter.put("/profile",response_model=ApiResponse[GuruWalasDetailWithSekolah],tags=["GURUWALAS/AUTH-PROFILE"])
+async def updateProfileguruWalas(body : UpdateProfileRequest = UpdateProfileRequest(),alamat : UpdateAlamatBody = UpdateAlamatBody(),guruWalas : dict = Depends(getWalasAuth),session : sessionDepedency = None) :
+    return await authProfileService.updateProfile(guruWalas["id"],body,alamat,session)
 
-# @petugasBKRouter.get("/absen/histori-tinjauan",response_model=ApiResponse[GetHistoriTinjauanAbsenResponse],tags=["PETUGASBK/ABSEN"])
-# async def getHistoriTinjauanAbsen(petugasBK : dict = Depends(getPetugasBKAuth),session : sessionDepedency = None) :
-#     return await absenService.getHistoriTinjauanAbsen(petugasBK["id"],session)
-
-# @petugasBKRouter.get("/absen/histori-tinjauan/{id_absen}",response_model=ApiResponse[GetHistoriTinjauanAbsenResponse],tags=["PETUGASBK/ABSEN"])
-# async def getHistoriTinajuanDetil(id_absen : int,session : sessionDepedency = None) :
-#     return await absenService.getDetailTinjauanAbsensiById(id_absen,session)
-
-# @petugasBKRouter.get("/absen/kelas-tinjauan",response_model=ApiResponse[GetAllKelasTinjauanResponse],tags=["PETUGASBK/ABSEN"])
-# async def getKelasTinjauan(petugasBK : dict = Depends(getPetugasBKAuth),session : sessionDepedency = None) :
-#     return await absenService.getAllKelasTinjauan(petugasBK["id"],session)
-
-# @petugasBKRouter.get("/absen/byKelas",response_model=ApiResponse[dict[str,dict[int,AbsenBase]]],tags=["PETUGASBK/ABSEN"])
-# async def getAbsenByKelas(query : GetAbsenByKelasFilterQuery = Depends(),session : sessionDepedency = None) :
-#     return await absenService.getAbsenByKelas(query,session)
-
-# @petugasBKRouter.get("/absen/bySiswa",response_model=ApiResponse[list[AbsenWithJadwalMapel]],tags=["PETUGASBK/ABSEN"])
-# async def getAbsenBySiswa(query : GetAbsenBySiswaFilterQuery = Depends(),session : sessionDepedency = None) :
-#     return await absenService.getAllAbsenBySiswa(query,session)
-
-# @petugasBKRouter.get("/absen/detail/{id_absen}",response_model=ApiResponse[GetAbsenHarianResponse],tags=["PETUGASBK/ABSEN"])
-# async def getDetailAbsen(id_absen : int,session : sessionDepedency = None) :
-#     return await absenService.getDetailAbsenHarian(id_absen,session)
-
-# @petugasBKRouter.patch("/absen/tinjau/{id_absen}",response_model=ApiResponse[TinjauAbsenResponse],tags=["PETUGASBK/ABSEN"])
-# async def getDetailAbsen(id_absen : int,body : TinjauAbsenRequest,petugasBK : dict = Depends(getPetugasBKAuth),session : sessionDepedency = None) :
-#     return await absenService.tinjauAbsen(petugasBK,id_absen,body,session)
+@guruWalasRouter.put("/profile/foto_profile",response_model=ApiResponse[GuruWalasBase],tags=["GURUWALAS/AUTH-PROFILE"])
+async def updateFotoProfileguruWalas(foto_profile : UploadFile,guruWalas : dict = Depends(getWalasAuth),session : sessionDepedency = None) :
+    return await authProfileService.add_update_foto_profile(guruWalas["id"],foto_profile,session)
